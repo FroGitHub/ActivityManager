64f58c6f91ff87922ce5fa1bbb33145e61a70213
FroGitHub <andrey8102310@gmail.com>
2025-05-17 19:00:17 +0300

    fix CI х2

--- src/test/java/back/activitymanager/ActivityManagerApplicationTests.java
+++ src/test/java/back/activitymanager/ActivityManagerApplicationTests.java
@@ -2,9 +2,14 @@ package back.activitymanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @SpringBootTest
 class ActivityManagerApplicationTests {
+
+	@MockBean
+	private UserDetailsService userDetailsService;
 
 	@Test
 	void contextLoads() {
