6e45e707af3c84c79e650b63e92599b5f2642d9f
FroGitHub <andrey8102310@gmail.com>
2025-07-17 02:13:40 +0300

    Add filter by LocalTime

--- src/main/java/back/activitymanager/repository/ActivityRepository.java
+++ src/main/java/back/activitymanager/repository/ActivityRepository.java
@@ -7,14 +7,18 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
     boolean existsByAuthor(User principal);
 
     @EntityGraph
+    @Query("SELECT a FROM Activity a WHERE a.isDeleted = false AND a.localDateTime > current_timestamp")
     Optional<Activity> findById(Long id);
 
+    @Query("SELECT a FROM Activity a WHERE a.isDeleted = false AND a.localDateTime > current_timestamp")
     Page<Activity> findByAuthorEmail(Pageable pageable, String email);
 
+    @Query("SELECT a FROM Activity a WHERE a.isDeleted = false AND a.localDateTime > current_timestamp")
     Page<Activity> findByParticipantsEmail(Pageable pageable, String name);
 }
