fd7614ec84a718ae772abe01f5641642064ed10a
FroGitHub <andrey8102310@gmail.com>
2025-07-31 13:15:49 +0300

    fix SecurityConfig for OpenAPI

--- src/main/java/back/activitymanager/config/SecurityConfig.java
+++ src/main/java/back/activitymanager/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
                                 .anyRequest()
                                 .authenticated()
                 )
-                .httpBasic(Customizer.withDefaults())
+                //.httpBasic(Customizer.withDefaults())
                 .sessionManagement(session
                         -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .userDetailsService(userDetailsService)
